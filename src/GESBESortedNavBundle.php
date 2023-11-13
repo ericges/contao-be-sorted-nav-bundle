9e5297df9f00d1bac3b3b7dbcb34fd1215e9de55
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2023-11-13 15:20:24 +0100

    Fixed: script integrity hash

--- src/GESBESortedNavBundle.php
+++ src/GESBESortedNavBundle.php
@@ -16,6 +16,11 @@ class GESBESortedNavBundle extends Bundle
 
     public static function getScript(): string
     {
-        return '<script type="text/javascript" src="/bundles/gesbesortednav/js/ges-be-sorted-nav.js" integrity="1449cbfd7a7210d94753506a15051db0a5b4ec35147201f58fd9408a883913cd80b919057bd35b3c6fb13d34e11f66d1"></script>';
+        $filePath = '/bundles/gesbesortednav/js/ges-be-sorted-nav.js';
+        # $hash = hash_file('sha384', __DIR__.'/../public/js/ges-be-sorted-nav.js', true);
+        # $hash = hex2bin('1449cbfd7a7210d94753506a15051db0a5b4ec35147201f58fd9408a883913cd80b919057bd35b3c6fb13d34e11f66d1');
+        # $encoded = base64_encode($hash);
+        $encoded = 'FEnL/XpyENlHU1BqFQUdsKW07DUUcgH1j9lAiog5E82AuRkFe9NbPG+xPTThH2bR';
+        return "<script type=\"text/javascript\" src=\"$filePath\" integrity=\"sha384-$encoded\"></script>";
     }
 }
